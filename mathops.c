222758c497eea413b115b29dbed74057ec92f3e3
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-16 06:21:39 -0800

    Adding div function

--- mathops.c
+++ mathops.c
@@ -78,7 +78,7 @@ void _div(stack_t **stack, unsigned int line_number)
 
 	if (!*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n",
+		fprintf(stderr, "L%d: can't div, stack too short\n",
 				line_number);
 		closefile();
 		freeline();
