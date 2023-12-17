7a751e5ccd7e01ce24df00dff2bd3dca2d3473e7
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-16 21:29:41 -0800

    Adding pchar function

--- printer.c
+++ printer.c
@@ -45,3 +45,35 @@ void pint(stack_t **stack, unsigned int line_number)
 	else
 		printf("%d\n", (*stack)->n);
 }
+
+/**
+ * pchar - prints the char at the top of the stack, followed by a new line.
+ * @stack: The singly linked list
+ * @line_number: The file line number
+ * Return: void
+ **/
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "can't pchar, stack empty\n",
+				line_number);
+		closefile();
+		freeline();
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n > 127 || (*stack)->n < 0)
+	{
+		fprintf(stderr, "can't pchar, value out of range",
+				line_number);
+		closefile();
+		freeline();
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
